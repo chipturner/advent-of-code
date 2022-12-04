1a4871b6b23daf2d11df9766c17b3650a1659b8d
Chip Turner <chip.turner@lacework.net>
2022-12-03 23:27:10 -0800

    day4p2

--- 2022/src/day4.py
+++ 2022/src/day4.py
@@ -13,10 +13,17 @@ def contains(l1, l2):
         return True
     return False
 
+def overlap(l1, l2):
+    if l1[0] <= l2[0] <= l1[1]:
+        return True
+    if l2[0] <= l1[0] <= l2[1]:
+        return True
+    return False
+
 def main() -> None:
     lines = helpers.read_input()
     for assignments in lines:
         a1, a2 = map(parse_assignment, assignments.split(','))
-        print(a1, a2, contains(a1, a2))
+        print(a1, a2, overlap(a1, a2))
 
 main()
