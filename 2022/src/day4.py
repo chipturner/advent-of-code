ba02ce500b3cf9a29b32083ddf27e4679038782f
Chip Turner <chip.turner@lacework.net>
2022-12-03 23:25:33 -0800

    day4p1

---
+++ 2022/src/day4.py
@@ -0,0 +1,22 @@
+import helpers
+
+import itertools
+import collections
+
+def parse_assignment(a):
+    return list(map(int, a.split('-')))
+
+def contains(l1, l2):
+    if l1[0] >= l2[0] and l1[1] <= l2[1]:
+        return True
+    if l2[0] >= l1[0] and l2[1] <= l1[1]:
+        return True
+    return False
+
+def main() -> None:
+    lines = helpers.read_input()
+    for assignments in lines:
+        a1, a2 = map(parse_assignment, assignments.split(','))
+        print(a1, a2, contains(a1, a2))
+
+main()
