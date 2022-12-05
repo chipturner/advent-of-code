a761cd18f9221828c50fe46e811365aac42d0710
Chip Turner <cturner@pattern.net>
2022-12-05 08:41:08 -0800

    tidy up and generalize parser just in case it comes back...

--- 2022/src/day5.py
+++ 2022/src/day5.py
@@ -1,43 +1,33 @@
-import pprint
-import helpers
+import sys
 
-import itertools
-import collections
 
-input = r"""
-            L   M           M     
-        D   R   Z           C   L 
-        C   S   T   G       V   M 
-R       L   Q   B   B       D   F 
-H   B   G   D   Q   Z       T   J 
-M   J   H   M   P   S   V   L   N 
-P   C   N   T   S   F   R   G   Q 
-Z   P   S   F   F   T   N   P   W 
-"""
+def parse_input():
+    lines = sys.stdin.read()
+    column_lines, moves = lines.split("\n\n")
+    return column_lines.strip("\n").split("\n"), moves.rstrip("\n").split("\n")
 
 
 def main() -> None:
+    column_lines, moves = parse_input()
+    column_count = (len(column_lines[-1]) + 1) // 4
+
     data = []
-    for i in range(9):
+    for i in range(column_count):
         data.append([])
-        for idx, l in enumerate(input.lstrip('\n').splitlines()):
-            ch = l[i * 4]
-            if ch != ' ':
+        for line in column_lines:
+            ch = line[i * 4 + 1]
+            if ch != " ":
                 data[-1].append(ch)
         data[-1].reverse()
-    print(data)
 
-    lines = helpers.read_input()
-    for line in lines:
-        cmds = line.split()
-        if not cmds or cmds[0] != 'move':
-            continue
+    for move in moves:
+        cmds = move.split()
+        assert cmds[0] == "move"
         n, f, t = int(cmds[1]), int(cmds[3]) - 1, int(cmds[5]) - 1
         chunk = data[f][-n:]
         data[f][-n:] = []
         data[t].extend(chunk)
-    pprint.pprint(data)
-    print(''.join(data[i][-1] for i in range(9)))
-            
-    
+    print("".join(data[i][-1] for i in range(column_count)))
+
+
 main()
