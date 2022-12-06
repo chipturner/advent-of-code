f94b749b193a6c0b78866033809e1e18e11dd28e
Chip Turner <cturner@pattern.net>
2022-12-05 21:07:39 -0800

    pt2

--- 2022/src/day6.py
+++ 2022/src/day6.py
@@ -8,10 +8,10 @@ def main() -> None:
     lines = helpers.read_input()
     print(lines)
     pkt = lines[0]
-    chunks = list(pkt[n:n+4] for n in range(len(pkt) - 4))
+    chunks = list(pkt[n:n+14] for n in range(len(pkt) - 14))
     for idx, chunk in enumerate(chunks):
-        if len(set(chunk)) == 4:
-            print(chunk, idx + 4)
+        if len(set(chunk)) == 14:
+            print(chunk, idx + 14)
 
 
 main()
