2fac7c64b194a928b9c4bf18a03d5d96c835d928
Chip Turner <cturner@pattern.net>
2022-12-07 21:28:34 -0800

    d8p2

--- 2022/src/day8.py
+++ 2022/src/day8.py
@@ -23,20 +23,28 @@ def hslice(grid, row, col):
             ret[1].append(grid[j][col])
     return ret
 
+def calc_score(val, seq):
+    ret = 0
+    for s in seq:
+        ret += 1
+        if val <= s:
+            break
+    return ret
+
 def main() -> None:
     lines = helpers.read_input_digit_grid(int)
     print(lines)
 
-    count = 0
     for row in range(len(lines)):
         for col in range(len(lines[0])):
             vs1, vs2 = vslice(lines, row, col)
+            vs1.reverse()
             hs1, hs2 = hslice(lines, row, col)
-            if (row == 0 or row == len(lines) - 1 or col == 0 or col == len(lines) - 1 or
-                lines[row][col] > max(vs1) or lines[row][col] >max(hs1) or
-                lines[row][col] > max(vs2) or lines[row][col] >max(hs2)):
-                count += 1
-                
-    print(count)
+            hs1.reverse()
+            score = calc_score(lines[row][col], vs1)
+            score *= calc_score(lines[row][col], vs2)
+            score *= calc_score(lines[row][col], hs1)
+            score *= calc_score(lines[row][col], hs2)
+            print(row, col, score)
             
 main()
