bba4abf39a01e5fd7bcd075a3d2f7dc82f05a2a5
Chip Turner <cturner@pattern.net>
2022-12-08 22:33:30 -0800

    pt2

--- 2022/src/day9.py
+++ 2022/src/day9.py
@@ -9,7 +9,6 @@ import collections
 
 def calc_t_pos(h_pos, t_pos):
     dist = numpy.linalg.norm(h_pos - t_pos)
-    print(h_pos, t_pos, dist)
     if dist < 2:
         return t_pos
     delta = numpy.sign(h_pos - t_pos)
@@ -21,15 +20,20 @@ def main() -> None:
 
     deltas = { 'U': numpy.array((0, 1)), 'D': numpy.array((0, -1)), 'L': numpy.array((-1, 0)), 'R': numpy.array((1, 0)) }
 
-    h_pos = numpy.array((0, 0))
-    t_pos = numpy.array((0, 0))
-    visited = set()
+    origin = numpy.array((0, 0))
+    origin.flags["WRITEABLE"] = False
+    positions = [origin]
+
+    tail_9_visited = set()
     for direction, distance in [ s.split() for s in lines ]:
         for i in range(int(distance)):
-            h_pos += deltas[direction]
-            t_pos = calc_t_pos(h_pos, t_pos)
-            print(h_pos, t_pos)
-            visited.add(tuple(t_pos))
-    print(len(visited))
+            if len(positions) < 10:
+                positions.append(origin)
+            positions[0] = positions[0] + deltas[direction]
+            for idx in range(1, len(positions)):
+                positions[idx] = calc_t_pos(positions[idx - 1], positions[idx])
+            if len(positions) == 10:
+                tail_9_visited.add(tuple(positions[-1]))
+    print(len(tail_9_visited))
 
 main()
