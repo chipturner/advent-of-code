39faf40c49f220aa22ec2b01522ed2bd3a2d5f9f
Chip Turner <cturner@pattern.net>
2022-12-08 22:33:51 -0800

    reformat

--- 2022/src/day9.py
+++ 2022/src/day9.py
@@ -7,6 +7,7 @@ import helpers
 import itertools
 import collections
 
+
 def calc_t_pos(h_pos, t_pos):
     dist = numpy.linalg.norm(h_pos - t_pos)
     if dist < 2:
@@ -14,18 +15,24 @@ def calc_t_pos(h_pos, t_pos):
     delta = numpy.sign(h_pos - t_pos)
     return t_pos + delta
 
+
 def main() -> None:
     lines = helpers.read_input()
     print(lines)
 
-    deltas = { 'U': numpy.array((0, 1)), 'D': numpy.array((0, -1)), 'L': numpy.array((-1, 0)), 'R': numpy.array((1, 0)) }
+    deltas = {
+        "U": numpy.array((0, 1)),
+        "D": numpy.array((0, -1)),
+        "L": numpy.array((-1, 0)),
+        "R": numpy.array((1, 0)),
+    }
 
     origin = numpy.array((0, 0))
     origin.flags["WRITEABLE"] = False
     positions = [origin]
 
     tail_9_visited = set()
-    for direction, distance in [ s.split() for s in lines ]:
+    for direction, distance in [s.split() for s in lines]:
         for i in range(int(distance)):
             if len(positions) < 10:
                 positions.append(origin)
@@ -35,5 +42,6 @@ def main() -> None:
             if len(positions) == 10:
                 tail_9_visited.add(tuple(positions[-1]))
     print(len(tail_9_visited))
+
 
 main()
