fc25c4f28739d036052cd9582ec17f1a300fbb37
Chip Turner <cturner@pattern.net>
2022-12-09 21:29:15 -0800

    tidy pt2

--- 2022/src/day10.py
+++ 2022/src/day10.py
@@ -2,36 +2,33 @@ from __future__ import annotations
 
 import helpers
 
-import itertools
-import collections
-
 
 def main() -> None:
     lines = helpers.read_input()
     pending_adds = []
     x = 1
-    strength_sum = 0
-    cycle_signals = dict()
     cycle = 0
     instructions = lines
     while instructions:
         cycle += 1
+
         next_instruction = instructions.pop(0)
         s = next_instruction.split()
         if len(pending_adds):
             x += pending_adds.pop(0)
-        strength = x * cycle
-        cycle_signals[cycle] = strength
-        print(cycle, x)
-        if s[0] == 'noop':
+        sprite_center = (cycle - 1) % 40
+        if x - 1 <= sprite_center <= x + 1:
+            print("#", end="")
+        else:
+            print(".", end="")
+        if cycle % 40 == 0:
+            print()
+        if s[0] == "noop":
             continue
         else:
-            assert s[0] == 'addx'
+            assert s[0] == "addx"
             pending_adds.extend((0, int(s[1])))
-            instructions.insert(0, 'noop')
-            
-    interesting = [ 20, 60, 100, 140, 180, 220 ]
-    print(sum(cycle_signals[i] for i in interesting))
+            instructions.insert(0, "noop")
 
 
 main()
