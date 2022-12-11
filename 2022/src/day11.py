30dce3ae91b6d35174205bca3fcf63e9b1035dde
Chip Turner <cturner@pattern.net>
2022-12-10 23:49:18 -0800

    pt2

--- 2022/src/day11.py
+++ 2022/src/day11.py
@@ -1,17 +1,16 @@
 from __future__ import annotations
 
-import helpers
-
-import itertools
-import collections
 import sys
 import parse
+import math
+
+from typing import List
 
 from dataclasses import dataclass
 
 monkey_fmt = r"""Monkey {num}:
   Starting items: {items}
-  Operation: {operation}
+  Operation: new = {operation}
   Test: divisible by {divisor}
     If true: throw to monkey {target_true}
     If false: throw to monkey {target_false}
@@ -33,7 +32,6 @@ class Monkey:
 def main() -> None:
     input_monkeys = sys.stdin.read()
     monkey_chunks = input_monkeys.split("\n\n")
-    print(monkey_chunks)
 
     monkeys = []
     for chunk in monkey_chunks:
@@ -47,24 +45,32 @@ def main() -> None:
             int(result["target_true"]),
             int(result["target_false"]),
         )
+        # monkey.operation = f'(({monkey.operation}) % {monkey.divisor})'
         print(monkey)
         monkeys.append(monkey)
+    modulo_reduction = math.lcm(*[monkey.divisor for monkey in monkeys])
 
-    for turn in range(1, 21):
+    for turn in range(1, 10001):
         for monkey in monkeys:
             while monkey.items:
                 monkey.inspections += 1
                 item = monkey.items.pop(0)
                 l = dict(old=item)
-                exec(monkey.operation, None, l)
-                new = l['new'] // 3
+                exec(f"new = ({monkey.operation})", None, l)
+                new = l["new"]
                 if (new % monkey.divisor) == 0:
-                    dest = monkey.target_true 
+                    dest = monkey.target_true
                 else:
                     dest = monkey.target_false
-                print(f'item {item} going to {dest} as {new}')
+                new = new % modulo_reduction
                 monkeys[dest].items.append(new)
-        print(monkeys)
-    business = sorted([ monkey.inspections for monkey in monkeys ], reverse=True)
+        print(f"Round {turn}")
+        for monkey in monkeys:
+            print(
+                f"Monkey {monkey.number} inspected items {monkey.inspections} times has {len(monkey.items)} items"
+            )
+    business = sorted([monkey.inspections for monkey in monkeys], reverse=True)
     print(business[0] * business[1])
+
+
 main()
