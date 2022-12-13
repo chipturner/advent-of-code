bdc6319ffebd6d928f8f66970edff2a1c2c11b3a
Chip Turner <cturner@pattern.net>
2022-12-12 21:54:50 -0800

    pt2

--- 2022/src/day13.py
+++ 2022/src/day13.py
@@ -2,18 +2,18 @@ from __future__ import annotations
 
 import sys
 
-import helpers
 
 import itertools
-import collections
+import functools
+
 
 def compare(l1, l2, indent=0):
     print(f'{" "*indent}- Compare {l1} vs {l2}')
 
     if type(l1) == int and type(l2) == list:
-        return compare([l1], l2, indent+1)
+        return compare([l1], l2, indent + 1)
     if type(l2) == int and type(l1) == list:
-        return compare(l1, [l2], indent+1)
+        return compare(l1, [l2], indent + 1)
 
     if type(l1) == int and type(l2) == int:
         if l1 < l2:
@@ -22,7 +22,7 @@ def compare(l1, l2, indent=0):
             return 0
         else:
             return 1
-    
+
     for l, r in itertools.zip_longest(l1, l2):
         if l is None:
             return -1
@@ -32,21 +32,19 @@ def compare(l1, l2, indent=0):
             return v
 
     return 0
-        
 
 
 def main() -> None:
-    lines = sys.stdin.read()
-    chunks = lines.strip().split('\n\n')
-    print(chunks)
+    lines = ("[[2]]\n[[6]]\n" + sys.stdin.read().replace("\n\n", "\n").strip()).split()
+    entries = [eval(s) for s in lines]
+    print(entries)
 
-    proper = []
-    for idx, chunk in enumerate(chunks):
-        left, right = [ eval(s) for s in chunk.split('\n') ]
-        print()
-        print(f'== Pair {idx+1} ==')
-        if compare(left, right) == -1:
-            proper.append(idx+1)
-    print(sum(proper))
+    entries.sort(key=functools.cmp_to_key(compare))
+    ret = 1
+    for idx, v in enumerate(entries):
+        if v == [[2]] or v == [[6]]:
+            ret *= idx + 1
+    print(ret)
+
 
 main()
