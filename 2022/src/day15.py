0c5f7646b4680c6a10e04241af2f2f97702ca339
Chip Turner <cturner@pattern.net>
2022-12-15 08:00:47 -0800

    pt2

--- 2022/src/day15.py
+++ 2022/src/day15.py
@@ -11,6 +11,7 @@ from dataclasses import dataclass
 
 import re
 
+
 @dataclass
 class Grid:
     grid: Dict[Tuple[int, int], str]
@@ -19,15 +20,15 @@ class Grid:
         x_min = sorted([p[0] for p in self.grid.keys()])[0]
         x_max = sorted([p[0] for p in self.grid.keys()])[-1]
         return range(x_min, x_max + 1)
-        
+
     def y_range(self):
         y_min = sorted([p[1] for p in self.grid.keys()])[0]
         y_max = sorted([p[1] for p in self.grid.keys()])[-1]
         return range(y_min, y_max + 1)
-        
+
     def print_grid(self):
         for y in self.y_range():
-            print(f'{y:02d}: ', end='')
+            print(f"{y:02d}: ", end="")
             for x in self.x_range():
                 print(self.get(x, y), end="")
             print()
@@ -38,49 +39,67 @@ class Grid:
     def set(self, x, y, v):
         self.grid[x, y] = v
 
+
 def dist(x0, y0, x1, y1):
     return abs(x0 - x1) + abs(y0 - y1)
 
-sensor_re = re.compile(r'Sensor at x=(.*?), y=(.*?): closest beacon is at x=(.*?), y=(.*?)$')
+
+def boundaries(discs):
+    for (x, y), d in discs.items():
+        delta = d + 1
+        pt = [x - delta, y]
+        while pt[0] < x:
+            yield tuple(pt)
+            pt[0] += 1
+            pt[1] -= 1
+        while pt[0] < x + delta:
+            yield tuple(pt)
+            pt[0] += 1
+            pt[1] += 1
+        while pt[0] > x:
+            yield tuple(pt)
+            pt[0] -= 1
+            pt[1] += 1
+        while pt[0] > x - delta:
+            yield tuple(pt)
+            pt[0] -= 1
+            pt[1] -= 1
+
+
+sensor_re = re.compile(
+    r"Sensor at x=(.*?), y=(.*?): closest beacon is at x=(.*?), y=(.*?)$"
+)
+
 
 def main() -> None:
     lines = helpers.read_input()
 
     g = Grid({})
     sensor_beacon = dict()
+    sensor_discs = dict()
     for line in lines:
         m = sensor_re.match(line)
-        s_x, s_y, b_x, b_y = [ int(v) for v in m.groups() ]
+        s_x, s_y, b_x, b_y = [int(v) for v in m.groups()]
         sensor_beacon[(s_x, s_y)] = (b_x, b_y)
-        g.set(s_x, s_y, 'S')
-        g.set(b_x, b_y, 'B')
-        d = dist(s_x, s_y, b_x, b_y) // 2 + 1
-        
-        g.set(s_x - d, s_y, '!')
-        g.set(s_x + d, s_y, '!')
-        g.set(s_x, s_y - d, '!')
-        g.set(s_x, s_y + d, '!')
+        g.set(s_x, s_y, "S")
+        g.set(b_x, b_y, "B")
+        d = dist(s_x, s_y, b_x, b_y)
+        sensor_discs[(s_x, s_y)] = d
 
-#    g.print_grid()
+    # g.print_grid()
 
-    target_y = 2000000
-    count = 0
-    print()
-#    print('xx: ', end='')
-    for x in g.x_range():
-        beacon_possible = True
-        pt = x, target_y
-        for sensor, beacon in sensor_beacon.items():
-            sensor_pt_distance = dist(*sensor, *pt)
-            sensor_beacon_distance = dist(*sensor, *beacon)
-#            print(pt, sensor_pt_distance, sensor_beacon_distance)
-            if pt != beacon and sensor_pt_distance <= sensor_beacon_distance:
-                beacon_possible = False
-#        print(beacon_possible and g.get(x, target_y) or '#', end='')
-        if not beacon_possible:
-            count += 1
-    print()
-    print(count)
-        
+    print(list(boundaries({(0, 0): 1})))
+
+    bounds = 4000000
+    for b in boundaries(sensor_discs):
+        if not (0 <= b[0] <= bounds and 0 <= b[1] <= bounds):
+            continue
+        for (x, y), diameter in sensor_discs.items():
+            if dist(x, y, *b) < diameter + 1:
+                break
+        else:
+            print(b, b[0] * 4000000 + b[1])
+            break
+
 
 main()
