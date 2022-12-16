1518c2d30f74ac5d9868e9965ea82d121d7b0c3d
Chip Turner <cturner@pattern.net>
2022-12-16 12:35:41 -0800

    pt2

--- 2022/src/day16.py
+++ 2022/src/day16.py
@@ -49,27 +49,57 @@ def main() -> None:
     print(simplified_valve_graph)
 
     visit_times = {"AA": 0}
-    print(dfs(simplified_valve_graph, valve_rates, visit_times, "AA"))
+    all_paths = dfs(simplified_valve_graph, valve_rates, visit_times, "AA")
+    scored_paths = sorted(
+        [
+            (
+                score_visits(valve_rates, path),
+                set(p for p in path.keys() if p != "AA"),
+                list(path.keys()),
+            )
+            for path in all_paths
+        ],
+        key=lambda p: p[0],
+        reverse=True,
+    )
+    for (score, path_set, path) in scored_paths:
+        print(f"{score} {path_set} {path}")
+
+    sorted_path_pairs = []
+    max_score = 0
+    for idx, (s1, p1s, p1) in enumerate(scored_paths):
+        print(idx, len(scored_paths))
+        for s2, p2s, p2 in scored_paths[idx:]:
+            if s1 + s2 > max_score and p1s.isdisjoint(p2s):
+                max_score = s1 + s2
+                sorted_path_pairs.append([s1 + s2, p1, p2])
+    sorted_path_pairs.sort(key=lambda p: p[0])
+    print(sorted_path_pairs[-1])
+    for p in sorted_path_pairs:
+        print(p[0], sorted(p[1]), sorted(p[2]))
+
+
+MINUTES_AVAILABLE = 26
 
 
 def score_visits(valve_rates, visit_times):
     score = 0
     for valve, time in visit_times.items():
-        if time < 30:
+        if time < MINUTES_AVAILABLE:
             rate = valve_rates[valve]
-            score += (30 - time) * rate
+            score += (MINUTES_AVAILABLE - time) * rate
     return score
 
 
 def dfs(graph, valve_rates, visit_times, cur_node):
     cur_time = visit_times[cur_node]
-    sub_scores = [score_visits(valve_rates, visit_times)]
+    ret = [visit_times.copy()]
     for nxt, nxt_time in graph[cur_node]:
-        if nxt not in visit_times and nxt_time + cur_time < 30:
+        if nxt not in visit_times and nxt_time + cur_time <= MINUTES_AVAILABLE:
             visit_times[nxt] = nxt_time + cur_time + 1
-            sub_scores.append(dfs(graph, valve_rates, visit_times, nxt))
+            ret.extend(dfs(graph, valve_rates, visit_times, nxt))
             del visit_times[nxt]
-    return max(sub_scores)
+    return ret
 
 
 main()
