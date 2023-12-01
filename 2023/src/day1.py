66f25593c6e153321a688ea8cc5503530f28bae1
Chip Turner <cturner@pattern.net>
2023-12-01 15:35:13 -0800

    pt2

--- 2023/src/day1.py
+++ 2023/src/day1.py
@@ -5,14 +5,24 @@ import helpers
 import itertools
 import collections
 
+words = list(enumerate("one two three four five six seven eight nine".split()))
 
 def main() -> None:
     lines = helpers.read_input()
     vals = []
     for line in lines:
-        digits = list(filter(lambda c: c.isdigit(), line))
-        val = digits[0] + digits[-1]
-        vals.append(int(val))
+        nums = []
+        for idx in range(len(line)):
+            for val_m_1, word in words:
+                str_val = str(val_m_1 + 1)
+                if line[idx:idx+len(word)] == word:
+                    nums.append(str_val)
+                if line[idx] == str_val:
+                    nums.append(str(val_m_1 + 1))
+        print(line, nums)
+        val = int(nums[0] + nums[-1])
+        vals.append(val)
+    print(vals)
     print(sum(vals))
 
 
