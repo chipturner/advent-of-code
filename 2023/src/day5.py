be5301e60e2eb918e73325f9ae5f134de6758d54
Chip Turner <cturner@pattern.net>
2023-12-06 00:26:49 -0800

    dumb multiprocessing for practice

--- 2023/src/day5.py
+++ 2023/src/day5.py
@@ -6,6 +6,8 @@ import itertools
 import collections
 import re
 
+import multiprocessing
+
 path = "seed soil fertilizer water light temperature humidity location".split()
 def find_path(paths, seed):
     #print('find_path', paths, seed)
@@ -62,15 +64,26 @@ def main() -> None:
     print('ttt', find_path(dicts, 897591494-100))
 
     seed_ranges = [ (s[0], s[0] + s[1] - 1) for s in zip(seeds[::2], seeds[1::2]) ]
-    i = 0
-    while i < seed_ranges[-1][0] + seed_ranges[-1][1]:
-        if i % 100000 == 0:
-            print(f'checking for seed {i}')
-        maybe_seed = backwards_find_path(dicts, i)
-        for seed_range in seed_ranges:
-            if seed_range[0] <= maybe_seed < seed_range[1]:
-                print(f"{i} {maybe_seed} {backwards_find_path(dicts, i)}")
-                raise ValueError('oops')
-        i += 1
+
+    def worker(num_workers, offset):
+        i = offset
+        while i < seed_ranges[-1][0] + seed_ranges[-1][1]:
+            if i % 100000 == offset:
+                print(f'checking for seed {i}')
+            maybe_seed = backwards_find_path(dicts, i)
+            for seed_range in seed_ranges:
+                if seed_range[0] <= maybe_seed < seed_range[1]:
+                    print(f"{i} {maybe_seed} {backwards_find_path(dicts, i)}")
+                    return
+            i += num_workers
+
+    pool_size = 64
+    w = []
+    for i in range(pool_size):
+        p = multiprocessing.Process(target=worker, args=(pool_size, i))
+        p.start()
+        w.append(p)
+    for x in w:
+        x.join()
 
 main()
