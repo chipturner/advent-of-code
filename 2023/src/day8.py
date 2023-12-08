5b1f5787c8ce9618246216cad9c5696a265ebee8
Chip Turner <cturner@pattern.net>
2023-12-07 21:32:04 -0800

    lcm

--- 2023/src/day8.py
+++ 2023/src/day8.py
@@ -3,8 +3,7 @@ from __future__ import annotations
 import helpers
 
 import itertools
-import collections
-import re
+import math
 
 step_idx = {"L": 0, "R": 1}
 
@@ -17,15 +16,19 @@ def main() -> None:
         m[line[0:3]] = line[7:10], line[12:15]
 
     c = 0
-    cur = "AAA"
-    for step in steps:
-        c += 1
-        print(cur, c, step)
-        idx = step_idx[step]
-        cur = m[cur][idx]
-        if cur == "ZZZ":
-            break
-    print(c)
+    curs = list([s for s in m if s[-1] == 'A'])
+    hop_counts = [0] * (len(curs))
+
+    for idx, cur in enumerate(curs):
+        c = 0
+        for step in steps:
+            c += 1
+            curs[idx] = m[curs[idx]][step_idx[step]]
+            if curs[idx][-1] == 'Z':
+                hop_counts[idx] = c
+                break
+        print(hop_counts)
+    print(math.lcm(*hop_counts))
 
 
 main()
