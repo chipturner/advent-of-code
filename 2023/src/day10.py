6e445e99725e974117147c5af8754f38b0a830fc
Chip Turner <cturner@pattern.net>
2023-12-10 12:16:07 -0800

    pt2

--- 2023/src/day10.py
+++ 2023/src/day10.py
@@ -18,6 +18,17 @@ steps = {
     'S': (),
 }
 
+fills = {
+    '|': [ (1, 0), (1, 1), (1, 2) ],
+    '-': [ (0, 1), (1, 1), (2, 1) ],
+    'F': [ (2, 1), (1, 1), (1, 2) ],
+    'J': [ (0, 1), (1, 1), (1, 0) ],
+    '7': [ (0, 1), (1, 1), (1, 2) ],
+    'L': [ (1, 0), (1, 1), (2, 1) ],
+    'S': [],
+    '.': [],
+}
+
 def main() -> None:
     g = helpers.read_input_digit_grid(str)
     for y in range(len(g)):
@@ -52,6 +63,52 @@ def main() -> None:
             todo.append(pt)
         visited.add(n)
 
+    g.setflags(write=True)
+
     print(max(v for v in seen_dist.values() if v != sys.maxsize))
+
+    big_g = np.full((3 * g.shape[0], 3 * g.shape[1]), '.')
+
+    for y in range(len(g)):
+        for x in range(len(g[0])):
+            if seen_dist[y, x] == sys.maxsize:
+                g[y, x] = '.'
+            fill_spots = fills[g[(y, x)]]
+            for (xd, yd) in fill_spots:
+                big_g[(3 * y + yd, 3 * x + xd)] = '#'
+    big_g[3 * start[0] + 1, 3 * start[1] + 1] = '#'
+
+    big_g[3 * start[0] + 2, 3 * start[1] + 1] = big_g[3 * start[0] + 3, 3 * start[1] + 1]
+    big_g[3 * start[0] + 0, 3 * start[1] + 1] = big_g[3 * start[0] - 1, 3 * start[1] + 1]
+
+    big_g[3 * start[0] + 1, 3 * start[1] + 2] = big_g[3 * start[0] + 1, 3 * start[1] + 3]
+    big_g[3 * start[0] + 1, 3 * start[1] + 0] = big_g[3 * start[0] + 1, 3 * start[1] - 1]
+
+
+    for r in g:
+        print(''.join(r))
+    for r in big_g:
+        print(''.join(r))
+
+    todo = [(0, 0)]
+    while todo:
+        cur = todo.pop(0)
+        for neigh in helpers.neighbors(big_g, cur[0], cur[1]):
+            if big_g[neigh] == '.':
+                todo.append(neigh)
+                big_g[neigh] = ' '
+    for r in big_g:
+        print(''.join(r))
+
+
+    c = 0
+    for y in range(len(g)):
+        for x in range(len(g[0])):
+            if big_g[3 * y + 1, 3 * x + 1] == '.' and g[y, x] == '.':
+                c += 1
+                g[y, x] = '#'
+    print(c)
+    for r in g:
+        print(''.join(r))
 
 main()
