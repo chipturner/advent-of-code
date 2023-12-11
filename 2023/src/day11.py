7108763642506c7a1693e5d5b4f46911b3f14177
Chip Turner <cturner@pattern.net>
2023-12-11 06:32:23 -0800

    day11

--- 2023/src/day11.py
+++ 2023/src/day11.py
@@ -8,43 +8,46 @@ import re
 import numpy
 import sys
 
+
 def dist(pt1, pt2):
     return abs(pt1[0] - pt2[0]) + abs(pt1[1] - pt2[1]), pt1, pt2
+
+
+def scale_gaps(gaps, v):
+    return (1000000 - 1) * sum([1 if v > g else 0 for g in gaps]) + v
+
 
 def main() -> None:
     g = helpers.read_input_digit_grid(str)
     print(g)
-    print('\n'.join(''.join(r) for r in g))
+    print("\n".join("".join(r) for r in g))
     print()
 
-    new = []
-    for r in g:
-        if all(ch == '.' for ch in r):
-            new.append(['.'] * len(r))
-        new.append(r)
-    g = numpy.array(new)
+    row_gaps, col_gaps = [], []
+
+    for idx, r in enumerate(g):
+        if all(ch == "." for ch in r):
+            row_gaps.append(idx)
     g = g.transpose()
 
-    new = []
-    for r in g:
-        if all(ch == '.' for ch in r):
-            new.append(['.'] * len(r))
-        new.append(r)
-    g = numpy.array(new)
+    for idx, r in enumerate(g):
+        if all(ch == "." for ch in r):
+            col_gaps.append(idx)
     g = g.transpose()
 
-    print('\n'.join(''.join(r) for r in g))
+    print(row_gaps, col_gaps)
 
     galaxies = set()
     for pt in numpy.ndindex(g.shape):
-        if g[pt] == '#':
-            galaxies.add(pt)
+        if g[pt] == "#":
+            new_pt = (scale_gaps(row_gaps, pt[0]), scale_gaps(col_gaps, pt[1]))
+            galaxies.add(new_pt)
 
-    dists = [ dist(pt1, pt2) for pt1, pt2 in  itertools.combinations(galaxies, 2) ]
+    dists = [dist(pt1, pt2) for pt1, pt2 in itertools.combinations(galaxies, 2)]
     shortest_paths = collections.defaultdict(lambda: sys.maxsize)
     for d, pt1, pt2 in dists:
         shortest_paths[(pt1, pt2)] = min(shortest_paths[(pt1, pt2)], d)
-    print(shortest_paths)
     print(sum(shortest_paths.values()))
+
 
 main()
