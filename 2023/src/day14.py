2d06f12535689bbcb8695015e9574af3174bf3f3
Chip Turner <cturner@pattern.net>
2023-12-14 07:23:29 -0800

    pt2

--- 2023/src/day14.py
+++ 2023/src/day14.py
@@ -5,15 +5,14 @@ import helpers
 import itertools
 import collections
 import re
+import numpy
 
+spins = { 'N': 0, 'W': 1, 'S': 2, 'E': 3 }
 
-def main() -> None:
-    grid = helpers.read_input_digit_grid(str)
-    print(grid)
-    grid.setflags(write=True)
-    helpers.print_numpy_grid(grid)
-    print()
-
+def roll(grid, d):
+    for _ in range(spins[d]):
+        grid = numpy.rot90(grid, k=-1)
+        
     for idx, row in enumerate(grid):
         if idx == 0:
             continue
@@ -28,7 +27,35 @@ def main() -> None:
                 new_idx -= 1
                 grid[new_idx, x_idx] = 'O'
 
+    for _ in range(spins[d]):
+        grid = numpy.rot90(grid, k=1)
+
+    return grid
+
+def main() -> None:
+    grid = helpers.read_input_digit_grid(str)
+    grid.setflags(write=True)
+
+    rotations = itertools.cycle('NWSE')
+    previously_seen = {}
+    n = 0
+    while n < 1000000000:
+        cur_str = grid.tobytes()
+        if cur_str in previously_seen:
+            period = n - previously_seen[cur_str]
+            print(f'hit at {n} with period {period}')
+            if n + period < 1000000000:
+                n += period
+                continue
+
+        previously_seen[cur_str] = n
+        for _ in range(4):
+            rot = next(rotations)
+            grid = roll(grid, rot)
+        print(rot, n)
+        n += 1
     helpers.print_numpy_grid(grid)
+
     t = 0
     for idx, row in enumerate(grid):
         dist = len(grid) - idx
