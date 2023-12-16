a92c1e3d05aff21b1882fd9dcb139155bb7ad4d8
Chip Turner <cturner@pattern.net>
2023-12-15 16:05:01 -0800

    d2

--- 2023/src/day15.py
+++ 2023/src/day15.py
@@ -20,4 +20,31 @@ def main() -> None:
         print(s, hasher(s))
     print(sum(hasher(s) for s in seq))
 
+    boxes = [[] for i in range(256)]
+    for s in seq:
+        lens, op = re.match(r'^(\w+)([-=]\d*)$', s).groups()
+        box = boxes[hasher(lens)]
+        if op == '-':
+            box[:] = [ (l, c) for l, c in box if l != lens ]
+        else:
+            assert op[0] == '='
+            val = int(op[-1])
+            print(s, lens, op, val)
+            for idx, (l, v) in enumerate(box):
+                if l == lens:
+                    box[idx] = (l, val)
+                    break
+            else:
+                 box.append((lens, val))
+
+    print(boxes)
+    tot = 0
+    for boxnum, lenses in enumerate(boxes):
+        for idx, (l, v) in enumerate(lenses):
+            fpow = (boxnum + 1) * (idx + 1) * v
+            print(boxnum + 1, idx + 1, v)
+            tot += fpow
+            print(fpow)
+    print(tot)
+
 main()
