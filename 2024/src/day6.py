6b69eab2406e9662fc71348f015ca96dfe322680
Chip Turner <cturner@pattern.net>
2024-12-05 23:00:58 -0800

    whew

--- 2024/src/day6.py
+++ 2024/src/day6.py
@@ -6,6 +6,7 @@ import helpers
 import itertools
 import collections
 import re
+import copy
 
 directions = '^>v<'
 next_directions = dict(itertools.pairwise(directions + '^'))
@@ -14,6 +15,7 @@ vel_map = dict(zip(directions, deltas))
 
 def walk_grid(grid, guard_ch, guard_pos):
     path = [(guard_ch, guard_pos)]
+    seen = set(path)
     try:
         while True:
             next_pos = guard_pos + vel_map[guard_ch]
@@ -21,7 +23,10 @@ def walk_grid(grid, guard_ch, guard_pos):
                 guard_ch = next_directions[guard_ch]
                 next_pos = guard_pos + vel_map[guard_ch]
             guard_pos = next_pos
+            if (guard_ch, guard_pos) in seen:
+                return None
             path.append((guard_ch, guard_pos))
+            seen.add((guard_ch, guard_pos))
     except IndexError:
         return path
 
@@ -34,12 +39,21 @@ def main() -> None:
             guard_pos = helpers.Coordinate(row, col)
             break
 
+    first_guard_ch, first_guard_pos = guard_ch, guard_pos
     grid[guard_pos] = 'X'
     path = walk_grid(grid, guard_ch, guard_pos)
     seen = set(path)
     print(len(set([d for _, d in seen])))
 
-
-
+    loop_makers = set()
+    for idx, (guard_ch, guard_pos) in enumerate(path[:-1]):
+        next_guard_ch, next_guard_pos = path[idx+1]
+        new_grid = copy.deepcopy(grid)
+        new_grid[next_guard_pos] = '#'
+        new_path = walk_grid(new_grid, first_guard_ch, first_guard_pos)
+        if not new_path:
+            print('yay', idx, next_guard_pos)
+            loop_makers.add(next_guard_pos)
+    print('loops', len(loop_makers))
 
 main()
