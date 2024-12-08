688000729de9d070ee7b9268af61634e0b94c206
Chip Turner <cturner@pattern.net>
2024-12-07 21:14:21 -0800

    noice

--- 2024/src/day8.py
+++ 2024/src/day8.py
@@ -5,7 +5,7 @@ import helpers
 import itertools
 import collections
 import re
-
+import math
 
 def main() -> None:
     grid = helpers.Grid.from_list_of_strings(helpers.read_input())
@@ -22,12 +22,16 @@ def main() -> None:
     for ch, locations in antennas.items():
         for location_tuple in itertools.combinations(locations, 2):
             delta = location_tuple[0] - location_tuple[1]
-            p1 = location_tuple[0] + delta
-            p2 = location_tuple[1] - delta
-            if grid.in_bounds(p1):
-                antinodes.add(p1)
-            if grid.in_bounds(p2):
-                antinodes.add(p2)
+            print('d1', delta)
+            delta = helpers.Coordinate(delta.row // abs(math.gcd(delta.row, delta.col)), delta.col // abs(math.gcd(delta.row, delta.col)))
+            print('d2', delta)
+            for i in range(-100, 100):
+                p1 = location_tuple[0] + delta * i
+                p2 = location_tuple[1] - delta * i
+                if grid.in_bounds(p1):
+                    antinodes.add(p1)
+                if grid.in_bounds(p2):
+                    antinodes.add(p2)
     print('anti', len(antinodes))
 
 
