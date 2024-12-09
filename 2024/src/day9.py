79c4d0b10a26ec83d372742d11756c6f5a181f8c
Chip Turner <cturner@pattern.net>
2024-12-08 22:31:28 -0800

    whew

--- 2024/src/day9.py
+++ 2024/src/day9.py
@@ -6,6 +6,19 @@ import itertools
 import collections
 import re
 
+def contiguous_size(block_layout, pt, direction):
+    orig_pt = pt
+    orig_id = block_layout[pt]
+    while 0 <= pt < len(block_layout) and orig_id == block_layout[pt]:
+        pt += direction
+    return abs(pt - orig_pt)
+
+def file_size(block_layout, pt):
+    file_ch = block_layout[pt]
+    orig_pt = pt
+    while pt < len(block_layout):
+        pt += 1
+    return pt - orig_pt
 
 def main() -> None:
     lines = helpers.read_input()
@@ -26,20 +39,25 @@ def main() -> None:
     read_pt = len(block_layout) - 1
 
     new_block_id_map = list(block_id_map)
-    while write_pt < read_pt:
-        if block_layout[write_pt] != '.':
-            write_pt += 1
-            continue
-        if block_layout[read_pt] == '.':
-            read_pt -= 1
-            continue
-        block_layout[write_pt] = block_layout[read_pt]
-        block_layout[read_pt] = '.'
-        new_block_id_map[write_pt] = block_id_map[read_pt]
-        new_block_id_map[read_pt] = 0
-        read_pt -= 1
-        write_pt += 1
-        # print(''.join(block_layout))
+    size_to_move = contiguous_size(new_block_id_map, read_pt, -1)
+
+    while read_pt > 0:
+        size_to_move = contiguous_size(new_block_id_map, read_pt, -1)
+        print('considering moving size ', size_to_move, 'at pt ', read_pt)
+        #print('before', ''.join(block_layout))
+        for write_pt in range(read_pt):
+            if block_layout[write_pt] == '.' and contiguous_size(new_block_id_map, write_pt, 1) >= size_to_move:
+                for _ in range(size_to_move):
+                    block_layout[write_pt] = block_layout[read_pt]
+                    block_layout[read_pt] = '.'
+                    new_block_id_map[write_pt] = block_id_map[read_pt]
+                    new_block_id_map[read_pt] = 0
+                    read_pt -= 1
+                    write_pt += 1
+                break
+        else:
+            read_pt -= size_to_move
+        #print('after ', ''.join(block_layout))
 
     print(sum(idx * v for idx, v in enumerate(new_block_id_map)))
 main()
--- 2024/src/day9bad.py
+++ 2024/src/day9bad.py
@@ -5,49 +5,30 @@ import helpers
 import itertools
 import collections
 import re
+from dataclasses import dataclass
 
+@dataclass
+class Block:
+    file_id: Optional[int]
+    offset: int
+    size: int
 
 def main() -> None:
     lines = helpers.read_input()
     block_spaces = [ int(v) for v in lines[0]]
     print(block_spaces)
     blocks, spaces = block_spaces[0::2], block_spaces[1::2]
-    print(blocks, spaces)
-    block_initial_offset = dict()
-
+    spaces.append(0)
+    l = []
     cur_offset = 0
-    space_indexes = []
-    for idx, (block, space) in enumerate(zip(blocks, spaces)):
-        block_initial_offset[idx] = cur_offset
-        cur_offset += block
-        for i in range(space):
-            space_indexes.append(cur_offset)
-            cur_offset += 1
-    print(block_initial_offset)
+    for idx in range(len(blocks)):
+        b = Block(idx, cur_offset, blocks[idx])
+        cur_offset += b.size
+        l.append(b)
+        b = Block(None, cur_offset, spaces[idx])
+        cur_offset += b.size
+        l.append(b)
 
-    s_iter = iter(spaces)
-    s_idx_iter = iter(space_indexes)
-    b_iter = reversed(list(enumerate(blocks)))
-
-    total = 0
-    avail = next(s_iter)
-
-    try:
-        for b_idx, b_size in b_iter:
-            while b_size > 0:
-                while avail > 0:
-                    avail -= 1
-                    b_size -= 1
-                    blocks[b_idx] = b_size
-                    avail_idx = next(s_idx_iter)
-                    total += b_idx * avail_idx
-                    print(f'putting file id {b_idx} into avail idx {avail_idx}')
-                if avail == 0:
-                    avail = next(s_iter)
-    except StopIteration:
-        pass
-    for b_idx, b_size in b_iter:
-        total += sum(b_idx * i for i in range(block_initial_offset[b_idx], block_initial_offset[b_idx] + b_size))
-    print(total)
+    
 
 main()
