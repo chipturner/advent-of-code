1ed3fe6d4a8692587c5239e898ae009fdcea74f4
Chip Turner <cturner@pattern.net>
2024-12-10 22:32:05 -0800

    grr

--- 2024/src/day11.py
+++ 2024/src/day11.py
@@ -2,11 +2,13 @@ from __future__ import annotations
 
 import helpers
 
+import more_itertools
 import itertools
 import collections
 import re
+import functools
 
-def blink(stone):
+def single_blink(stone):
     if stone == '0':
         return ['1']
 
@@ -15,20 +17,23 @@ def blink(stone):
 
     return [str(int(stone) * 2024)]
 
-def blink_all(stones):
-    ret = []
-    for stone in stones:
-        ret.extend(blink(stone))
-    return ret
+def blink_all(num_blinks, stones):
+    for i in range(num_blinks):
+        stones = list(more_itertools.flatten(single_blink(stone) for stone in stones))
+    return stones
+
+@functools.lru_cache(maxsize=None)
+def count_stones(num_blinks, stones):
+    if num_blinks == 1:
+        return len(stones)
+    blinked = tuple(single_blink(s) for s in stones)
+    return sum(count_stones(num_blinks - 1, tuple(s)) for s in blinked)
 
 def main() -> None:
     lines = helpers.read_input()
-    stones = lines[0].split(' ')
-
-    for blink in range(27):
-        # print('blink', blink, ' '.join(stones))
-        print('blink', blink, len(stones))
-        stones = blink_all(stones)
-
-
+    stones = tuple(lines[0].split(' '))
+    for bn in range(1, 100):
+        res = count_stones(bn, stones)
+        print(bn-1, res)
+    print(len(res))
 main()
