f6b57014a8c44f2c52c25eafb3a9fbe66530be77
Chip Turner <cturner@pattern.net>
2024-12-12 05:07:35 -0800

    whew

--- 2024/src/day12.py
+++ 2024/src/day12.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import helpers
+from helpers import up, down, left, right
 
 import itertools
 import collections
@@ -15,6 +16,73 @@ def perimiter(grid, region):
                 fence.append(n)
     return fence
 
+def edges(grid, region, perimiter):
+    region_pts = {r[0] for r in region}
+    perimiter = set(perimiter)
+    ret = 0
+
+    for row in range(grid.nrows):
+        col = 0
+        wall_checks = []
+        while 0 <= col < grid.ncols:
+            probe = helpers.Coordinate(row, col)
+            #print('c', probe, probe in perimiter, probe + down in region_pts)
+            walking_wall = probe in perimiter and probe + down in region_pts
+            wall_checks.append(walking_wall)
+            col += 1
+        #print(wall_checks)
+        deltas = sum(a != b for a, b in itertools.pairwise(wall_checks))
+        walls = deltas // 2
+        #print(deltas, walls)
+        ret += walls
+
+    for row in range(grid.nrows):
+        col = grid.ncols - 1
+        wall_checks = []
+        while 0 <= col < grid.ncols:
+            probe = helpers.Coordinate(row, col)
+            #print('c', probe, probe in perimiter, probe + up in region_pts)
+            walking_wall = probe in perimiter and probe + up in region_pts
+            wall_checks.append(walking_wall)
+            col -= 1
+        #print(wall_checks)
+        deltas = sum(a != b for a, b in itertools.pairwise(wall_checks))
+        walls = deltas // 2
+        #print(deltas, walls)
+        ret += walls
+
+    for col in range(grid.ncols):
+        row = 0
+        wall_checks = []
+        while 0 <= row < grid.nrows:
+            probe = helpers.Coordinate(row, col)
+            #print('c', probe, probe in perimiter, probe + right in region_pts)
+            walking_wall = probe in perimiter and probe + right in region_pts
+            wall_checks.append(walking_wall)
+            row += 1
+        #print(wall_checks)
+        deltas = sum(a != b for a, b in itertools.pairwise(wall_checks))
+        walls = deltas // 2
+        #print(deltas, walls)
+        ret += walls
+
+    for col in range(grid.ncols):
+        row = grid.nrows - 1
+        wall_checks = []
+        while 0 <= row < grid.nrows:
+            probe = helpers.Coordinate(row, col)
+            #print('c', probe, probe in perimiter, probe + left in region_pts)
+            walking_wall = probe in perimiter and probe + left in region_pts
+            wall_checks.append(walking_wall)
+            row -= 1
+        #print(wall_checks)
+        deltas = sum(a != b for a, b in itertools.pairwise(wall_checks))
+        walls = deltas // 2
+        #print(deltas, walls)
+        ret += walls
+
+    return ret
+            
 def main() -> None:
     grid = helpers.Grid.from_list_of_strings(helpers.read_input()).pad('.')
     grid.print()
@@ -37,8 +105,8 @@ def main() -> None:
             f[pos] = ch
         print()
         f.print()
-        # print(f[r[0][0]], len(r), len(p))
-        s += len(r) * len(p)
+        print(f[r[0][0]], len(r), len(p), edges(grid, r, p))
+        s += len(r) * edges(grid, r, p)
     print(s)
 
 main()
