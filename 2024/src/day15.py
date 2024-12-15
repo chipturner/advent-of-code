ae690709360c2ab8936b3440c1088c8af22a028c
Chip Turner <cturner@pattern.net>
2024-12-14 21:47:20 -0800

    pt2

--- 2024/src/day15.py
+++ 2024/src/day15.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import helpers
 
+import copy
 import itertools
 import collections
 import re
@@ -10,35 +11,58 @@ from helpers import up, right, down, left
 
 move_map = dict(zip('^>v<', (up, right, down, left)))
 
+def make_move(grid, pos, move):
+    probe = pos + move
+    if grid[probe] == '.':
+        grid[probe] = grid[probe - move]
+        grid[probe - move] = '.'
+        return True
+    if grid[probe] == '#':
+        return False
+    if move in (up, down):
+        if grid[probe] == '[':
+            if make_move(grid, pos + move, move) and make_move(grid, pos + move + right, move):
+                grid[probe] = grid[probe - move]
+                grid[probe - move] = '.'
+                return True
+        if grid[probe] == ']':
+            if make_move(grid, pos + move, move) and make_move(grid, pos + move + left, move):
+                grid[probe] = grid[probe - move]
+                grid[probe - move] = '.'
+                return True
+        return False
+    # left/right
+    if make_move(grid, pos + move, move):
+        grid[probe] = grid[probe - move]
+        grid[probe - move] = '.'
+        return True
+    return False
+
 def main() -> None:
     lines = "\n".join(helpers.read_input())
     c1, c2 = lines.split('\n\n')
-    grid = helpers.Grid.from_list_of_strings(c1.split('\n'))
+    grid = helpers.Grid.from_list_of_strings(c1.replace('#', '##').replace('O','[]').replace('.', '..').replace('@', '@.').split('\n'))
     moves = list(''.join(c2.split('\n')))
 
     pos = grid.find_all('@')[0]
     while moves:
-        print('move', moves[0], pos)
         grid.print()
-        print('done')
+        print('move', moves[0], pos)
         print()
         move = move_map[moves.pop(0)]
+        saved = copy.deepcopy(grid)
+        if make_move(grid, pos, move):
+            grid[pos] = '.'
+            pos = pos + move
+            grid[pos] = '@'
+        else:
+            grid = saved
 
-        probe = move + pos
-        while grid[probe] == 'O':
-            probe += move
-        if grid[probe] == '#':
-            continue
-
-        while probe != pos:
-            grid[probe] = grid[probe - move]
-            probe -= move
-        grid[pos] = '.'
-        pos += move
+    grid.print()
 
     score = 0
     for pos, ch in grid.items():
-        if ch == 'O':
+        if ch == '[':
             score += pos.row * 100 + pos.col
     print(score)
 
